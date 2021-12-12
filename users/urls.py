579f08459bf3f56d90662beed1b4d52bf0dcb8d3
Mwila Mwale <lala@Mwilas-MacBook-Pro.local>
2021-12-12 18:23:17 +0200

    Added alan sdk

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path, include
 from . import views
 
-
 urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
-    path('dashboard/', views.dashboard, name="dashboard"), 
+    path('', views.Dashboard.as_view(), name="dashboard"),
     path('register/', views.Register.as_view(), name="register"),
     path('oauth/', include('social_django.urls')),
 ]
--- users/views.py
+++ users/views.py
@@ -1,17 +1,24 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse
 from django.contrib.auth import login
 from django.views import View
 from users.forms import CustomUserCreationForm 
+from analyzer.models import Conversation
 
 
-def dashboard(request):
-    return render(request, 'users/dashboard.html')
+# def dashboard(request):
+    # return render(request, 'users/dashboard.html')
+
+class Dashboard(LoginRequiredMixin, View):
+    def get(self, request):
+        conversations = Conversation.objects.filter(owner=request.user)
+        return render(request, "users/dashboard.html", {"conversations": conversations})
 
 
 class Register(View):
     def get(self, request):
-        return render(request, "users/register.html", {"form": CustomUserCreationForm } )
+        return render(request, "users/register.html", {"form": CustomUserCreationForm})
 
     def post(self, request):
         form = CustomUserCreationForm(request.POST)
